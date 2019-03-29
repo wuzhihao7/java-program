f3418e47dabd71e751fa7f1c12e361ecdbe37fdc
wuzhihao7 <wuzhihao7@outlook.com>
2019-03-29 18:13:19 +0800

    nio

--- nio/src/test/java/com/keehoo/channel/ChannelDemo.java
+++ nio/src/test/java/com/keehoo/channel/ChannelDemo.java
@@ -6,7 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 
 /**
  * @author wuzhihao
@@ -16,11 +19,15 @@ import java.nio.channels.FileChannel;
 public class ChannelDemo {
     @Test
     public void testChannelToBuffer() {
-        try (RandomAccessFile file = new RandomAccessFile("C:\\job\\workspace\\idea\\java-samples\\nio\\src\\test\\resources\\channel-read.txt", "r")) {
-            //利用channel中的FileChannel来实现文件读取
-            FileChannel channel = file.getChannel();
+        try (RandomAccessFile file = new RandomAccessFile("C:\\job\\workspace\\idea\\java-samples\\nio\\src\\test\\resources\\channel-read.txt", "r");
+             //利用channel中的FileChannel来实现文件读取
+             FileChannel channel = file.getChannel();) {
+
+            Charset charset = Charset.forName("UTF-8");
+            CharsetDecoder decoder = charset.newDecoder();
             //设置缓冲区容量
-            ByteBuffer byteBuffer = ByteBuffer.allocate(2);
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            CharBuffer charBuffer = CharBuffer.allocate(1024);
             //从通道中读取数据到缓冲区，返回读取的字节数量
             int read = channel.read(byteBuffer);
             //数量为-1表示读取完毕
@@ -28,14 +35,18 @@ public class ChannelDemo {
                 System.out.print("read:" + read + ":");
                 //切换模式为读模式，其实就是把position的位置设置为0，可以从0开始读取
                 byteBuffer.flip();
+                decoder.decode(byteBuffer, charBuffer, false);
+                charBuffer.flip();
                 //如果缓冲区还有数据
-                while (byteBuffer.hasRemaining()) {
+                while (charBuffer.hasRemaining()) {
                     //输出缓冲区内容
-                    System.out.print(((char) byteBuffer.get()));
+//                    System.out.print(((char) byteBuffer.get()));
+                    System.out.print(charBuffer.get());
                 }
                 System.out.println();
                 //数据读取完毕后清空缓冲区,（直接把position复位为0，可以直接覆盖内容）
                 byteBuffer.clear();
+                charBuffer.clear();
                 //继续把通道内剩余数据写入缓冲区
                 read = channel.read(byteBuffer);
             }
