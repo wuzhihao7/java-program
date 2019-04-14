c4ac601a5e559cb11d42534c095cf6f1932b4184
wuzhihao7 <wuzhihao7@outlook.com>
2019-04-14 12:08:54 +0800

    并发

---
+++ concurrent/src/main/java/com/keehoo/thread/SynchronizedDemo.java
@@ -0,0 +1,31 @@
+package com.keehoo.thread;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author wuzhihao
+ * @version V1.0
+ * @since 2019/4/14
+ */
+public class SynchronizedDemo implements Runnable {
+    private static int count;
+    @Override
+    public void run() {
+        synchronized (SynchronizedDemo.class){
+            IntStream.range(0, 10).forEach(i -> count++);
+        }
+    }
+
+    public static void main(String[] args) {
+        IntStream.range(0,10).forEach(i -> {
+            Thread t = new Thread(new SynchronizedDemo());
+            t.start();
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println("result: " + count);
+    }
+}
