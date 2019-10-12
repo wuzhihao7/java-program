00dbb675f23d4f99bb7ea5c929a416e557c418ea
wuzhihao7 <wuzhihao7@outlook.com>
2019-10-12 23:23:40 +0800

    completableFuture

---
+++ concurrent/src/main/java/com/keehoo/completablefuture/ExceptionallyDemo.java
@@ -0,0 +1,30 @@
+package com.keehoo.completablefuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * 使用 exceptionally() 回调处理异常 exceptionally()回调给你一个从原始Future中生成的错误恢复的机会。你可以在这里记录这个异常并返回一个默认值。
+ */
+public class ExceptionallyDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Integer age = -1;
+
+        CompletableFuture<String> maturityFuture = CompletableFuture.supplyAsync(() -> {
+            if(age < 0) {
+                throw new IllegalArgumentException("Age can not be negative");
+            }
+            if(age > 18) {
+                return "Adult";
+            } else {
+                return "Child";
+            }
+        }).exceptionally(ex -> {
+            System.out.println("Oops! We have an exception - " + ex.getMessage());
+            return "Unknown!";
+        });
+
+        System.out.println("Maturity : " + maturityFuture.get());
+
+    }
+}
