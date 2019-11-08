dcded4193d45be3217192cce0c932b6bd0451599
wuzhihao <wuzhihao@eptok.com>
2019-11-08 15:43:21 +0800

    quartz

--- quartz/src/main/java/com/house/HelloJob.java
+++ quartz/src/main/java/com/house/HelloJob.java
@@ -3,10 +3,13 @@ package com.house;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloJob implements Job {
+    private static final Logger log = LoggerFactory.getLogger(HelloJob.class);
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println("hello, quartz!");
+        log.info("hello, quartz!");
     }
 }
--- quartz/src/main/java/com/house/QuartzTest.java
+++ quartz/src/main/java/com/house/QuartzTest.java
@@ -2,6 +2,8 @@ package com.house;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
