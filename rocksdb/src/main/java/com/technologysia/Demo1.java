fde6daa157308ba4f2acbe1f87cfbb23e752d5fe
house <wuzhihao7@outlook.com>
2019-11-14 21:35:43 +0800

    rocksdb java demo

--- rocksdb/src/main/java/com/technologysia/Demo1.java
+++ rocksdb/src/main/java/com/technologysia/Demo1.java
@@ -9,16 +9,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Demo1 {
-    static {
-        // a static method that loads the RocksDB C++ library.
-        RocksDB.loadLibrary();
-    }
     public static void main(String[] args) {
         // the Options class contains a set of configurable DB options
         // that determines the behaviour of the database.
         try(final Options options = new Options().setCreateIfMissing(true)) {
             // a factory method that returns a RocksDB instance
-            try(final RocksDB db = RocksDB.open(options, "rocksdb")){
+            try(final RocksDB db = RocksDB.open(options, "rocksdb_data")){
                 db.put("hello".getBytes(), "world".getBytes());
                 byte[] bytes = db.get("hello".getBytes());
                 System.out.println(new String(bytes));
