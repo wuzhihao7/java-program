82d4eb31aaeaa11b4c86b8d1cd0699ab41efe869
wuzhihao <wuzhihao@eptok.com>
2020-01-21 17:57:57 +0800

    异步任务编排测试

--- concurrent/src/main/java/com/technologysia/completablefuture/demo1/AsyncDemo.java
+++ concurrent/src/main/java/com/technologysia/completablefuture/demo1/AsyncDemo.java
@@ -4,25 +4,34 @@ import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class AsyncDemo {
     public static void main(String[] args) throws IOException {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        CompletableFuture<Integer> f = new CompletableFuture<Integer>();
+//        CompletableFuture<Integer> f = new CompletableFuture<>();
 
-        new Thread(() -> {
-            // 子线程A启动
-            System.out.println("子线程A启动");
+        CompletableFuture<Void> f = CompletableFuture.runAsync(() -> {
+            System.out.println("模拟耗时任务");
             try {
-                System.out.println("子线程A沉睡5s");
-                Thread.sleep(5000L);
+                TimeUnit.SECONDS.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("子线程A令future完成");
-            f.complete(100);  // 当子线程A执行到f.complete的时候，会去看是否有注册好的f的then或者when（非async的），如果有的话，会顺次去执行。
-            System.out.println("子线程A结束");
-        }).start();;
+        });
+
+//        new Thread(() -> {
+//            // 子线程A启动
+//            System.out.println("子线程A启动");
+//            try {
+//                System.out.println("子线程A沉睡5s");
+//                Thread.sleep(5000L);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("子线程A令future完成");
+////            f.complete(100);  // 当子线程A执行到f.complete的时候，会去看是否有注册好的f的then或者when（非async的），如果有的话，会顺次去执行。
+//            System.out.println("子线程A结束");
+//        }).start();
 
 
         // 当前线程（主线程）执行到这里的时候，如果子线程还没有执行到f.complete(100)，
@@ -30,7 +39,7 @@ public class AsyncDemo {
         // 哪个线程就负责执行whenComplete的内容。
         // 如果当前线程（主线程）执行到这里的时候，f.complete(100)已经被其他线程执行完毕了。
         // 那么只有当前线程自己来执行whenComplete里面的内容了。
-        f.whenCompleteAsync((i, ex) -> {
+        f.whenComplete((i, ex) -> {
             // 这个场景下，whenComplete的回调的执行线程会是子线程A
             System.out.println("do something after complete begin");
             try {
@@ -40,10 +49,7 @@ public class AsyncDemo {
                 e.printStackTrace();
             }
             System.out.println("do something after complete end");
-        }, executorService);
-
-
+        });
         System.out.println("main over");
-//        System.in.read();
     }
 }
--- concurrent/src/main/java/com/technologysia/threadpool/ThreadPoolDemo.java
+++ concurrent/src/main/java/com/technologysia/threadpool/ThreadPoolDemo.java
@@ -3,7 +3,6 @@ package com.technologysia.threadpool;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import java.util.concurrent.*;
-import java.util.stream.IntStream;
 
 /**
  * 反面教材，线程池资源耗尽，线程全部阻塞
@@ -13,53 +12,146 @@ public class ThreadPoolDemo {
         ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
                 .setNameFormat("consumer-queue-thread-%d").build();
 
-        ExecutorService pool = new ThreadPoolExecutor(10, 10, 60L, TimeUnit.SECONDS,
+        ExecutorService pool = new ThreadPoolExecutor(2, 10, 60L, TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(128),namedThreadFactory,new ThreadPoolExecutor.AbortPolicy());
+        new Thread(() -> {
+            ThreadPoolExecutor tpe = ((ThreadPoolExecutor) pool);
+            while (true) {
+                System.out.println();
 
-        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(IntStream.range(0, 20).mapToObj(i -> CompletableFuture.runAsync(() -> {
-//            System.out.println("1_" + i + ":" + Thread.currentThread().getName());
-            //统计交易方发生额
-            CompletableFuture<Void> cf1 = CompletableFuture.runAsync(() -> {
-//                System.out.println("2_" + i + ":" + Thread.currentThread().getName());
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }, pool);
-            //统计账务方发生额
-            CompletableFuture<Void> cf2 = CompletableFuture.runAsync(() -> {
-//                System.out.println("3_" + i + ":" + Thread.currentThread().getName());
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }, pool);
-            //交易方和账务方统计完成后，进行合并
-            cf1.thenCombineAsync(cf2, (tradeAccountDayMap, accountingAccountDayMap) -> {
-//                System.out.println("4_" + i + ":" + Thread.currentThread().getName());
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                int queueSize = tpe.getQueue().size();
+                System.out.println("当前排队线程数：" + queueSize);
 
-                return null;
-            }, pool).whenComplete((o, throwable) -> {
+                int activeCount = tpe.getActiveCount();
+                System.out.println("当前活动线程数：" + activeCount);
+
+                long completedTaskCount = tpe.getCompletedTaskCount();
+                System.out.println("执行完成线程数：" + completedTaskCount);
+
+                long taskCount = tpe.getTaskCount();
+                System.out.println("总线程数：" + taskCount);
+
                 try {
-                    TimeUnit.SECONDS.sleep(1);
+                    TimeUnit.SECONDS.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            });
-//            System.out.println("5_" + i + ":" + Thread.currentThread().getName());
-        }, pool)).toArray(CompletableFuture[]::new));
-        voidCompletableFuture.whenCompleteAsync((aVoid, throwable) -> System.out.println("外部计算完成")).join();
-        System.out.println(Thread.currentThread().getName());
+            }
+        }).start();
+
+        new ThreadStat("主方法", Thread.currentThread()).start();
+        CompletableFuture<Void> f1 = CompletableFuture.runAsync(() -> async(pool), pool);
+        CompletableFuture<Void> f2 = CompletableFuture.runAsync(() -> async2(pool), pool);
+
+        CompletableFuture<Void> all = CompletableFuture.allOf(f1, f2);
+        all.join();
+
+
+//        while (true){
+//            if(all.isDone()) break;
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+        System.out.println("全部完成");
+
         pool.shutdown();
         while (!pool.awaitTermination(1, TimeUnit.SECONDS)) {
+            System.out.println("等待...");
         }
 
+    }
+
+    private static void async(ExecutorService pool) {
+        new ThreadStat("1-async", Thread.currentThread()).start();
+        CompletableFuture<Void> f1 = CompletableFuture.runAsync(() -> {
+            new ThreadStat("1-async,1",Thread.currentThread()).start();
+            try {
+                TimeUnit.SECONDS.sleep(10);
+                System.out.println("1-模拟耗时1");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, pool);
+        CompletableFuture<Void> f2 = CompletableFuture.runAsync(() -> {
+            new ThreadStat("1-async,2",Thread.currentThread()).start();
+            try {
+                TimeUnit.SECONDS.sleep(10);
+                System.out.println("1-模拟耗时2");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, pool);
+
+        CompletableFuture<Void> all = CompletableFuture.allOf(f1, f2);
+//        while (true){
+//            if(all.isDone()) break;
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        System.out.println("1-异步完成");
+    }
+
+
+    private static void async2(ExecutorService pool) {
+        new ThreadStat("2-async",Thread.currentThread()).start();
+        CompletableFuture<Void> f1 = CompletableFuture.runAsync(() -> {
+            new ThreadStat("2-async,1",Thread.currentThread()).start();
+            try {
+                TimeUnit.SECONDS.sleep(10);
+                System.out.println("2-模拟耗时1");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, pool);
+        CompletableFuture<Void> f2 = CompletableFuture.runAsync(() -> {
+            new ThreadStat("2-async,2",Thread.currentThread()).start();
+            try {
+                TimeUnit.SECONDS.sleep(10);
+                System.out.println("2-模拟耗时2");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, pool);
+
+        CompletableFuture<Void> all = CompletableFuture.allOf(f1, f2);
+
+//        while (true){
+//            if(all.isDone()) break;
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        System.out.println("2-异步完成");
+    }
+}
+
+class ThreadStat extends Thread{
+    private String trace;
+    private Thread thread;
+
+    public ThreadStat(String trace, Thread thread){
+        this.trace = trace;
+        this.thread = thread;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            System.err.println(trace+":"+thread.getName() +"---"+ thread.getState());
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
